7f9a3ff1b7207153e43403200fb2513e71e77de4
Alfi74 <catwyzle@ymail.com>
2023-10-17 10:59:36 +0200

    p1

---
+++ src/js/main.js
@@ -0,0 +1,65 @@
+"use strict";
+
+const haushaltsbuch = {
+    gesamtbilanz: {
+        einnahmen: 0,
+        ausgaben: 0,
+        bilanz: 0,
+    },
+
+    neuer_eintrag: {
+        titel: null,
+        typ: null,
+        betrag: null,
+        datum: null
+    },
+
+    eintrag_erfassen(){
+        this.neuer_eintrag.titel = prompt('Titel:');
+        this.neuer_eintrag.typ = prompt("Typ (Einnahme oder Ausgabe");
+        this.neuer_eintrag.betrag = parseInt(prompt('Betrag (in Cent)'));
+        this.neuer_eintrag.datum = prompt('Datum(jjjj-mm-tt)');
+    },
+
+    eintrag_ausgeben() {
+        console.log(`Titel: ${this.neuer_eintrag.titel}
+        Typ: ${this.neuer_eintrag.typ}
+        Betrag: ${this.neuer_eintrag.betrag} ct
+        Datum: ${this.neuer_eintrag.datum}`
+        );
+    },
+
+    eintrag_mit_gesamtbilanz_verrechnen() {
+        switch(this.neuer_eintrag.typ){
+            case 'Einnahme':
+                this.gesamtbilanz.einnahmen += this.neuer_eintrag.betrag;
+                this.gesamtbilanz.bilanz += this.neuer_eintrag.betrag;
+                break;
+            case 'Ausgabe':
+                this.gesamtbilanz.ausgaben += this.neuer_eintrag.betrag;
+                this.gesamtbilanz.bilanz -= this.neuer_eintrag.betrag;
+                break;
+            default:
+                console.log(`Der Typ "${this.neuer_eintrag.typ}" ist nicht bekannt!`);
+                break;
+        }
+    },
+
+    gesamtbilanz_ausgeben() {
+        console.log(`
+        Einnahmen: ${this.gesamtbilanz.einnahmen} ct
+        Ausgaben: ${this.gesamtbilanz.ausgaben} ct
+        Bilanz: ${this.gesamtbilanz.bilanz} ct
+        Bilanz: ${this.gesamtbilanz.bilanz >= 0}`)
+    },
+
+    eintrag_hinzufuegen() {
+        this.eintrag_erfassen();
+        this.eintrag_ausgeben();
+        this.eintrag_mit_gesamtbilanz_verrechnen();
+        this.gesamtbilanz_ausgeben();
+    }
+};
+
+haushaltsbuch.eintrag_hinzufuegen();
+haushaltsbuch.eintrag_hinzufuegen();
